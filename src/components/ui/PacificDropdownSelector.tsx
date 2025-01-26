10fd5449174f576271b457c5aa70366afe38bcb2
Fahim Ahmed <ahmed.fahim37@gmail.com>
2025-01-26 18:43:53 +0600

    fix

--- src/components/ui/PacificDropdownSelector.tsx
+++ src/components/ui/PacificDropdownSelector.tsx
@@ -24,13 +24,8 @@ const PacificDropdownSelector = ({
       value={selectedValue}
       onValueChange={(val) => onValueChange(val)} // Update the state on selection
     >
-<<<<<<< HEAD
-      <SelectTrigger className="bg-primary rounded-[8px] text-[#F5F5F5]">
-        <SelectValue placeholder={selectedValue} />
-=======
       <SelectTrigger className="bg-primary rounded-[8px] text-white">
         <SelectValue placeholder={selectedValue || placeholderText} />
->>>>>>> 4235ba63ba00a89f402129f9014b9c0e7ce1a563
       </SelectTrigger>
       <SelectContent className="w-fit">
         <SelectGroup>

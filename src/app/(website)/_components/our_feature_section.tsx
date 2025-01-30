65deb8bc2569044fb30e4b06574f5cfb4f36facf
Monir Hossain Abde Rabby <monirhrabby.personal@gmail.com>
2025-01-30 17:58:28 +0600

    fix: products

--- src/app/(website)/_components/our_feature_section.tsx
+++ src/app/(website)/_components/our_feature_section.tsx
@@ -1,59 +1,86 @@
 "use client";
 
+// Packages
+import { useQuery } from "@tanstack/react-query";
+import { motion } from "framer-motion";
+import { useSession } from "next-auth/react";
+import { useInView } from "react-intersection-observer";
+
+
 // local import
 import { fadeIn } from "@/components/animations/variant";
 import { ButtonArrow } from "@/components/shared/button/ButtonArrow";
 import FeaturedProductCard from "@/components/shared/cards/featured_card";
 import SectionHeading from "@/components/shared/SectionHeading/SectionHeading";
-import { useQuery } from "@tanstack/react-query";
-import { motion } from "framer-motion";
-import { useInView } from "react-intersection-observer";
+import ProductCardSkeleton from "@/components/shared/skeletons/productCardSkeleton";
+import ErrorContainer from "@/components/ui/error-container";
+import SkeletonWrapper from "@/components/ui/skeleton-wrapper";
+import { ProductResponse } from "@/types/product";
 
-const fetchProducts = async () => {
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/product`,
-  );
-  if (!response.ok) {
-    throw new Error("Network error");
-  }
-  return response.json();
-};
+
 
 const OurFeatureSection = () => {
+  const session = useSession();
+  const token = session?.data?.user?.token;
   const [ref, inView] = useInView({
     threshold: 0.2,
     triggerOnce: true,
   });
 
   //? // Fetch products
-  const { data, error, isLoading } = useQuery({
+  const { data, isError, error, isLoading } = useQuery<ProductResponse>({
     queryKey: ["products"],
-    queryFn: fetchProducts,
+    queryFn: async () => {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/product`, {
+          headers: {
+            'Authorization': `Bearer ${token}`
+          }
+        });
+      if (!response.ok) {
+        throw new Error("Network error");
+      }
+      return response.json();
+    },
   });
+  
   const products = data?.data;
 
+  let content;
+
+
   if (isLoading) {
-    return <div>Loading...</div>;
+    content = <div className="grid grid-cols-1 gap-[17px] pt-[40px] md:grid-cols-3 md:gap-[27px] lg:grid-cols-4">
+      {[1,2,3,4,5,67,8].map((n) => (
+        <ProductCardSkeleton key={n} />
+      ))}
+    </div>
   }
 
-  if (error) {
-    return <div>Error: {error.message}</div>;
+  else if (isError) {
+    content = <ErrorContainer message={error.message} />
+  } else if (products && products.length === 0 ) {
+    content = <div>NO Product Found</div>
+  } else if (products && products.length > 0) {
+    content = <motion.div
+    variants={fadeIn("up", 0.3)}
+    initial="hidden"
+    animate={inView ? "show" : "hidden"} // Start animation only when in view
+    viewport={{ once: false, amount: 0.3 }}
+    className="grid grid-cols-1 gap-[17px] pt-[40px] md:grid-cols-3 md:gap-[27px] lg:grid-cols-4"
+  >
+    {products?.map((product: any) => (
+      <SkeletonWrapper isLoading={false} key={product._id}>
+        <FeaturedProductCard  product={product} />
+      </SkeletonWrapper>
+    ))}
+  </motion.div>
   }
   return (
     <motion.div ref={ref} className="section container py-[80px]">
       <SectionHeading heading="Our Featured Products" subheading="Products" />
 
-      <motion.div
-        variants={fadeIn("up", 0.3)}
-        initial="hidden"
-        animate={inView ? "show" : "hidden"} // Start animation only when in view
-        viewport={{ once: false, amount: 0.3 }}
-        className="grid grid-cols-1 gap-[17px] pt-[40px] md:grid-cols-3 md:gap-[27px] lg:grid-cols-4"
-      >
-        {products.map((product: any) => (
-          <FeaturedProductCard key={product._id} product={product} />
-        ))}
-      </motion.div>
+      {content}
 
       <div className="mt-[40px] flex w-full items-center justify-center">
         <ButtonArrow text="Explore More" size="sm" href="/products" />

045b60c47476dfb8a7440e3b448d95b55f3c74b9
sozibhossain <sozibhossain69@mail.com>
2025-01-30 11:17:47 +0600

    Contact page

--- src/app/(website)/contact/page.tsx
+++ src/app/(website)/contact/page.tsx
@@ -1,3 +1,4 @@
+import { PageHeader } from "@/components/shared/sections/page-header";
 import ContactForm from "./_components/ContactForm";
 import ContactInfo from "./_components/ContactInfo";
 import ContactMap from "./_components/ContactMap";
@@ -5,6 +6,19 @@ import ContactMap from "./_components/ContactMap";
 const Contact: React.FC = () => {
   return (
     <div>
+      <PageHeader
+                  title="Contact Us"
+                  items={[
+                    {
+                    label: "Home",
+                    href: "/",
+                  },
+                  {
+                  label: "Contact Us",
+                  href: "/contact",
+                },
+              ]}
+            />
       <div className="flex flex-col items-stretch md:flex-row justify-between gap-[30px] container section ">
         <div className="order-2 md:order-none max-h-full">
           <ContactInfo />

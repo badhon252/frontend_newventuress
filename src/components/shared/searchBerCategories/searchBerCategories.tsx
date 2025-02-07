41434d37b1b7d34115db522a56d42788e5eed8af
amitchackroborty04 <amitck803@gmail.com>
2025-02-07 16:32:25 +0600

    responsive searchbar

--- src/components/shared/searchBerCategories/searchBerCategories.tsx
+++ src/components/shared/searchBerCategories/searchBerCategories.tsx
@@ -7,12 +7,20 @@ import ThemeSwitcher from './theme-switcher';
 function SearchBerCategories() {
     return (
         <div className='bg-[#E6EEF6] dark:bg-[#482D721A] py-[10px]'>
-            <div className='container  gap-8 flex items-center justify-between'>
-               
+            <div className=' container  flex  gap-x-[20px] lg:justify-between   '>
+                <div className='w-[178px] lg:w-[15%]'>
                     <Categories />
+                </div>
+                <div className='w-[100%] lg:w-[60%] '>
                     <SearchBer />
-                
+                </div>
+                <div className=' w-[254px] lg:w-[18%] '>
                     <ThemeSwitcher />
+                </div>
+
+
+
+
             </div>
         </div>
     )
--- src/components/shared/searchBerCategories/theme-switcher.tsx
+++ src/components/shared/searchBerCategories/theme-switcher.tsx
@@ -4,14 +4,17 @@ import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { useTheme } from "next-themes";
 
 const ThemeSwitcher = () => {
-    const {theme, setTheme} = useTheme()
+  const { theme, setTheme } = useTheme()
   return (
-    <Tabs defaultValue={theme} value={theme} className="w--fit bg-transparent" onValueChange={(theme) => setTheme(theme)}>
-    <TabsList className="border-[1px] border-[#152764] dark:border-[#6841A5] py-1">
-      <TabsTrigger value="light" className="text-[#152764] data-[state=active]:text-white ">CBD/HEMP</TabsTrigger>
-      <TabsTrigger value="dark" className="data-[state=active]:bg-[#4A2E74] text-[#6841A5] data-[state=active]:text-white ">Recreational</TabsTrigger>
-    </TabsList>
-  </Tabs>
+    <Tabs defaultValue={theme} value={theme} className="bg-transparent " onValueChange={(theme) => setTheme(theme)}>
+      <TabsList className="border-[1px] border-[#152764] dark:border-[#6841A5] py-1 h-[44px] w-[204px]   ">
+
+        <TabsTrigger value="light" className="text-[#152764] data-[state=active]:text-white  ">CBD/HEMP</TabsTrigger>
+
+        <TabsTrigger value="dark" className="data-[state=active]:bg-[#4A2E74] text-[#6841A5] data-[state=active]:text-white ">Recreational</TabsTrigger>
+
+      </TabsList>
+    </Tabs>
   )
 }
 
